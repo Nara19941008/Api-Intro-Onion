4613a23a9d8ee27c2ed9bcee1f32a8a86963d4cd
Nara19941008 <narminim@code.edu.az>
2022-11-16 00:02:42 +0300

    Added bio table

---
+++ ApiIntro/ApiIntro/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIntro.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNameById(int id)
+        {
+            string name = "Narmin" + id;
+            return Ok(name);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<string> names = new List<string>() { "Seid", "Orxan", "Narmin", "Fidan", "Seadet" };
+            return Ok(names);
+        }
+        
+    }
+}
---
+++ App/RepositoryLayer/Repositories/Repository.cs
@@ -0,0 +1,59 @@
+﻿using RepositoryLayer.Repositories.Interfaces;
+using DomainLayer.Common;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _entities;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _entities = _context.Set<T>();
+        }
+
+        public async Task Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> Get(int id)
+        {
+            T entity = await _entities.FindAsync(id) ?? throw new NullReferenceException();
+            return entity;
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<List<T>> GetAll(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IRepository<T> where T : BaseEntity
+    {
+    }
+}
